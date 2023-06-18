7cd5007775aa9877db00b877a6d94b5d838e8e04
filin-md <filin.md9@gmail.com>
2023-06-18 14:40:20 +0300

    remove init, remove setter

--- src/keyboard.py
+++ src/keyboard.py
@@ -4,29 +4,19 @@ from src.item import Item
 
 class MixinLang:
 
-    def __init__(self):
-        super().__init__()
-        self.__language = "EN"
+    __language = "EN"
 
     @property
     def language(self):
         return self.__language
 
-    @language.setter
-    def language(self, lang):
-        if lang == "RU" or lang == "EN":
-            self.__language = lang
-        else:
-            raise AttributeError ("property 'language' of 'KeyBoard' object has no setter")
 
     def change_lang(self):
         if self.language == "RU":
-            self.language = "EN"
+            self.__language = "EN"
         elif self.language == "EN":
-            self.language = "RU"
+            self.__language = "RU"
         return self
-
-
 
 
 class Keyboard(Item, MixinLang):
